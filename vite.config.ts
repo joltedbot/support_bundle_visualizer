d19e1294339c92c9285ffe6b62501fdc1112b649
Dave White <dwhite@joltedweb.com>
2026-03-20 11:29:54 -0700

    fix: import defineConfig from vitest/config to allow test block in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 import { readFileSync } from 'node:fs'
